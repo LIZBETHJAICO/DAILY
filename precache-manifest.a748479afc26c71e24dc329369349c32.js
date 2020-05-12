a90e9ffe45ab064e4f97b9184f2fc31c71fd3762
LIZBETHJAICO <lizbethjaico@gmail.com>
2020-05-12 00:52:27 -0500

    Updates

---
+++ precache-manifest.a748479afc26c71e24dc329369349c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b150befceeea4ca94d353c0349a5f4",
+    "url": "/DAILY/index.html"
+  },
+  {
+    "revision": "1ff487974eb1fff80a3a",
+    "url": "/DAILY/static/css/main.c212f166.chunk.css"
+  },
+  {
+    "revision": "ce808adfbeb5fd1319b1",
+    "url": "/DAILY/static/js/2.33d012c5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DAILY/static/js/2.33d012c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff487974eb1fff80a3a",
+    "url": "/DAILY/static/js/main.9ff4355a.chunk.js"
+  },
+  {
+    "revision": "78bd5eafbdde0d369176",
+    "url": "/DAILY/static/js/runtime-main.a79be295.js"
+  }
+]);
